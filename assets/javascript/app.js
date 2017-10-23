ba3e87162929f67e549c7a16235a58f64b6e7b86
Claire Pate <clairepate@Claires-Air.attlocal.net>
2017-10-23 16:16:52 -0400

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,96 +1,79 @@
+
+$("#start").on("click", function(){
+	$("#start").remove();
+	game.loadQuestion();
+})
+
+
+
+
+
 var storedCorrect = [];
 var storedIncorrect = [];
 var storedUnAnswered = [];
 
-var questions = {
-		question1:'Actress Gal Gadot starred in what american superhero film released in 2017?',
-			
-		question2:'Who played the female lead in the thriller "V for Vendetta"?',
+var questions = [{
+		questionAsked:'Actress Gal Gadot starred in what american superhero film released in 2017?',
+		answers:["Wonder Woman", "Avengers", "Ant Man", "Suicide Squad"],
+		correctAnswer: "Wonder Woman"
+	}, {
+
+		questionAsked:'Who played the female lead in the thriller "V for Vendetta"?',
+		answers: ["Selma Hayek", "Keira Knightley", "Natalie Portman", "Amy Schumer"],
+		correctAnswer: "Natalie Portman"
+	}, {  
+		questionAsked:'Tyler Durden is a fictional character appearing as the central protagonist and antagonist in what 1999 American Film?',
+		answers:["Boondock Saints", "Fight Club", "Zoolander", "Mrs. Doubtfire"],
+		correctAnswer: "Fight Club"
+	}, {
+		questionAsked:"In Disney's 'The Litte Mermaid' what is the name of the human Ariel fell in love with?",
+		answers: ["John Smith", "Prince Charming", "Prince Eric", "Prince Philip"],
+		correctAnswer: "Prince Eric"
+	}, {
+		questionAsked:'In movie ratings what do the letters "PG" stand for?',
+		answers: ["Parental Gossip", "Pretty Good", "Powerfully Great", "Parental Guidance"],
+		correctAnswer: "Parental Guidance"
+	}, {
+		questionAsked:"The first movie of the Fast and the Furious franchise was released in what year?",
+		answers: ["2001", "1999", "2002", "2000"],
+		correctAnswer: "2001"
+	}, {
+		questionAsked:"In the 2016 american fantasy adventure file 'The Jungle Book', what is the name of the orphaned human boy?",
+		answers: ["Shere Khan", "Mowli", "Baloo", "Akela",
+		correctAnswer: "Mowli"
+	}, {
+		questionAsked:"In the movie 'The Wizard of Oz', what did the scarecrow want from the wizard?",
+		answers: ["Courage", "A soul", "A heart", "A brain",
+		correctAnswer: "A brain"
+	}];
+
+
 		
-		question3:'Tyler Durden is a fictional character appearing as the central protagonist and antagonist in what 1999 American Film?',
-		
-		question4:"In Disney's 'The Litte Mermaid' what is the name of the human Ariel fell in love with?",
-		
-		question5:'In movie ratings what do the letters "PG" stand for?',
-		
-		question6:"The first movie of the Fast and the Furious franchise was released in what year?",
-			
-		question7:"In the 2016 american fantasy adventure file 'The Jungle Book', what is the name of the orphaned human boy?",
-		
-		question8:"In the movie 'The Wizard of Oz', what did the scarecrow want from the wizard?"
 	
-		};
 
-var answer1 = [
-		"Wonder Woman",
-		"Avengers",
-		"Ant Man",
-		"Suicide Squad"
-	];
-
-var answer2 = [
-		"Selma Hayek",
-		"Keira Knightley",
-		"Natalie Portman",
-		"Amy Schumer"
-	];
-
-var answer3 = [
-		"Boondock Saints",
-		"Fight Club",
-		"Zoolander",
-		"Mrs. Doubtfire"
-	];
-
-var answer4 = [
-		"John Smith",
-		"Prince Charming",
-		"Prince Eric",
-		"Prince Philip"
-	];
-
-var answer5 = [
-		"Parental Gossip",
-		"Pretty Good",
-		"Powerfully Great",
-		"Parental Guidance"
-	];
-
-var answer6 = [
-		"2001",
-		"1999",
-		"2002",
-		"2000"
-	];
-
-var answer7 = [
-		"Shere Khan",
-		"Mowgli",
-		"Baloo",
-		"Akela"
-	];
-
-var answer8 = [
-		"Courage",
-		"A soul",
-		"A heart",
-		"A brain"
-	];
 
 
 $(document).ready(function(){
 
-$("#question").html(questions.question1);
+// 	$("#start").on("click", function(){
+// 		$("#wrapper").load("index.html");
+
+// });
+
+
+// });
+
+$("#question").html(question1.questionAsked);
 
 	
-	$(".btn-block1").html(answer1[0]);
-	$(".btn-block2").html(answer1[1]);
-	$(".btn-block3").html(answer1[2]);
-	$(".btn-block4").html(answer1[3]);
+	$(".btn-block1").html(["question1"] ["answer"] ["answer1"]["answerText"]);
+	// $(".btn-block2").html(answer1[1]);
+	// $(".btn-block3").html(answer1[2]);
+	// $(".btn-block4").html(answer1[3]);
 
-	console.log(answer1[0]);
+	// console.log(answer1[0]);
 
-});
+ });
 
 
 
