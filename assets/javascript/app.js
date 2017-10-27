66d8ed077d3738a44040f355dc64c4aefdaf6e02
Claire Pate <clairepate@Claires-Air.T-mobile.com>
2017-10-27 19:39:37 -0400

    finished game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,8 @@
 
 $(document).ready(function(){
+	$(".panel-default1").hide();
+
+	$(".panel-default2").hide();
 	
 	var counter = 45;
 	var correct = 0;
@@ -7,91 +10,75 @@ $(document).ready(function(){
 	var unanswered = 0; 
 	var intervalId;
 
-	$(".panel-default1").hide();
-	// $("#timeLeft").hide();
-	// $(".content").hide();
-	// $("#submit").hide();
-	$(".panel-default2").hide();
-	// $("#results").hide();
-	// $("#restart").hide();
-
 // show the questions when start is clicked/ start clock
 	$("#start").on("click", function(){
-	 	// $("#start").hide();
 	 	$(".panel-default").hide();
 	 	run();
 	 	$(".panel-default1").show();
-	 // 	$("#timeLeft").show();
-		// $(".content").show();
-		// $("#submit").show();
-		
-		
-	})
+	
+	});
 
 // show results when submit is clicked
    $("#submit").on("click", function(){
    		$(".panel-default1").hide();
-		// $(".content").hide();
-		// $("#submit").hide();
 		$(".panel-default2").show();
-		// $("#results").show();
-		// $("#restart").show();
 		stop();
-
-	})
+		$("timeLeft").html("<h3> Time Remaining: " + counter + "</h3>");
+		answers();
+	});
 	
 
 	function run() {
       intervalId = setInterval(decrement, 1000);
     }
 
-     function decrement() {
+
+    function decrement() {
       counter--;
       $("#timeLeft").html("<h3> Time Remaining: " + counter + "</h3>");
 
-      if (counter === 0) {
-        stop();
-      }
+      	if (counter === 0) {
+        	stop();
+        	$(".panel-default1").hide();
+			$(".panel-default2").show();
+			results();
+      	}
     }
+
 
 	function stop() {
       clearInterval(intervalId);
 	   
-	   	$(".panel-default1").hide();
-		// $(".content").hide();
-		// $("#submit").hide();
-		$(".panel-default2").show();
-		// $("#results").show();
-		// $("#restart").show();
     }
 
+// collect radio button input
     $('input[name=radio]').on("change", function(){
     	correct = $("input[value=true]:checked").length;
 		incorrect = $("input[value=false]:checked").length;
 		unanswered = (8 - (correct + incorrect));
+	  });
 
+// print to page	
+	function answers (){
 		$("#correct").html("Number Correct: " + correct);
 		$("#incorrect").html("Number Incorrect: " + incorrect);
 		$("#unanswered").html("Unsnswered Questions: " + unanswered);
-    })
+    };
 
+// resetting the game
     $("#restart").on("click", function(){
-    	$(".panel-default").hide();
-    	// $("#start").hide();
-    	// $("#timeLeft").show();
-    	$(".panel-default1").show();
-		// $(".content").show();
-		// $("#submit").show();
+    	$(".panel-default").show();
+    	$(".panel-default1").hide();
 		$(".panel-default2").hide();
-		// $("#results").hide();
-		// $("#restart").hide();
-		$("input:checked").removeAttr("checked");
+		$("input:checked").prop("checked", false);
+		stop();
 
-		var counter = 45;
-		var correct = 0;
-		var incorrect = 0;
-		var unanswered = 0;
-	})
+		counter = 45;
+		correct = 0;
+		incorrect = 0;
+		unanswered = 0; 
+		
+	});
 
 
 
